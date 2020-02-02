33537c96439c6e8c69b56297607e757f61b69267
Шадрин Владислав <v_shadreeen@mail.ru>
2020-02-02 21:43:11 +0600

    Начало проекта положено!

---
+++ Assets/Scripts/CharacterInventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInventory : MonoBehaviour
+{
+
+	public GameObject firstGun;
+	public GameObject secondGun;
+
+	//public Transform rightHand;
+	//public Transform leftHand;
+
+	private bool isTwoHanded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) {
+        	isTwoHanded = !isTwoHanded;
+			secondGun.SetActive(isTwoHanded);
+		}
+
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Movement : MonoBehaviour {
+
+	public GameObject player;
+	public AudioClip step;
+
+	public int speedRotation = 3;
+	public int speed = 5;
+	public int jumpSpeed = 350;
+
+	//private float delay = 3.5f;
+	//private float currentTime = 0f;
+	//private float indent = 0.1f;
+
+
+	void Start () { 
+		player = (GameObject)this.gameObject; 
+	}
+
+	void Update() {
+		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) { 
+			player.transform.position += player.transform.forward * speed * Time.deltaTime;
+		} 
+
+		if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) { 
+			player.transform.position -= player.transform.forward * speed * Time.deltaTime; 
+		} 
+
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) { 
+			player.transform.position -= player.transform.right * speed * Time.deltaTime; 
+		} 
+
+		if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) { 
+			player.transform.position += player.transform.right * speed * Time.deltaTime; 
+		} 
+
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			player.GetComponent<Rigidbody>().AddForce(player.transform.up * jumpSpeed * Time.deltaTime, ForceMode.Impulse);
+			GetComponent<AudioSource>().PlayOneShot(step);
+			//player.transform.position += player.transform.up * jumpSpeed * Time.deltaTime; 
+		} 
+	}
+
+}
