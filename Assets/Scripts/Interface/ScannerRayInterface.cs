aba0e3f93cf4eb73c03542552a68f08ac4983dda
Vladislav Shadrin <v_shadreeen@mail.ru>
2020-04-01 02:52:56 +0600

    Добавлен компонент, отвечающий за рейкаст

---
+++ Assets/Scripts/Interface/ScannerRayInterface.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScannerRayInterface : MonoBehaviour
+{
+    public Camera camera;
+    public Text textDisplay;
+    public float visibleDistance = 1000;
+    
+    private RaycastHit hit;
+    private Transform hitObjectTransform;
+
+    void Update() {
+        if (Physics.Raycast(camera.transform.position, camera.transform.TransformDirection(Vector3.forward), out hit, visibleDistance)) {
+            // Get the transform reference to the object we hit
+            hitObjectTransform = hit.transform;
+            if (hitObjectTransform.tag.Equals("WeaponMagazine"))
+            {
+                WeaponMagazine magazine = hitObjectTransform.gameObject.GetComponent<WeaponMagazine>();
+                textDisplay.text = magazine.magazine.ammoType + " ammo: " + magazine.magazine.currentAmount + 
+                                   "\nTake: 'E'";
+                if (Input.GetButtonDown("Fire1")) {
+                    magazine.gameObject.SetActive(false);
+                }
+            } else {
+                textDisplay.text = hitObjectTransform.name;
+            }
+        } else {
+            textDisplay.text = "";
+        }
+    }
+
+}
---
+++ Assets/Scripts/WeaponMagazine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class WeaponMagazine : MonoBehaviour
+{
+    [SerializeField] public AmmoMagazineContainer magazine;
+
+    private void Start()
+    {
+        magazine = new AmmoMagazineContainer();
+    }
+
+}
+
+[Serializable]
+public class AmmoMagazineContainer
+{
+    public enum AmmoType
+    {
+        AK,
+        LASER,
+        PLASMA
+    }
+    
+    public AmmoType ammoType;
+    public int capacity = 30;
+    public int currentAmount = 30;
+}
