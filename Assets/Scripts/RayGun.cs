00d8c618206089337c14cd829c37298dde427e0b
zyryanovvlad <vladislav.zur@gmail.com>
2020-04-03 02:04:36 +0600

    Изменён скрипт RayGun, добавлена трассировка пули, добавлен префаб TracerParticle

--- Assets/Scripts/RayGun.cs
+++ Assets/Scripts/RayGun.cs
@@ -1,21 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class RayGun : MonoBehaviour {
     
     public Camera fpsCam;
     public int damage = 10;
     public float shootingRange = 100f;
-    
     [Range(0.1f, 10f)] 
     public float delayBetweenShots = 1;
+    public GameObject TraceBullet;    // Particle for RayGun
     
     private float currentTimeForShot;
     private float timeStep = 0.1f;
     private RaycastHit hit;
     private CharacterHealth characterHealth;
-    
+
     // Update is called once per frame
     void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+            TraceBullet.gameObject.SetActive(true);
+            
+        }else{TraceBullet.gameObject.SetActive(false);
+    }
+
+               
         if (Input.GetKey(KeyCode.Mouse0) && currentTimeForShot >= delayBetweenShots)
         {
             Shoot();
@@ -35,6 +45,7 @@ public class RayGun : MonoBehaviour {
                 characterHealth.TakeDamage(damage);
             }
             Debug.Log(hit.transform.name);
+            
         }
     }
 }
