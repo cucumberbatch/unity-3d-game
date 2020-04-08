03e5f3a6657a8117579566e44d0fb3155d966336
Vladislav Shadrin <v_shadreeen@mail.ru>
2020-04-09 02:18:09 +0600

    Добавлена сцена для разработки и тестирования ИИ

---
+++ Assets/Scripts/EnemySphere.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemySphere : MonoBehaviour
+{
+    public Transform predator;
+    public GameObject hidingSpot;
+    public float speed = 1;
+    
+    [Header("State Materials")] 
+    public Material onSteadyMaterial;
+    public Material onAttackMaterial;
+
+    private bool _isAttack;
+    private Rigidbody _rigidbody;
+    private MeshRenderer _meshRenderer;
+
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        Vector3 impulse = 0.0001f * (transform.position - hidingSpot.GetComponent<HidingSpot>().ChooseCover(transform.position, predator.position).position);
+        
+        _rigidbody.AddForce(impulse, ForceMode.Impulse);
+    }
+}
