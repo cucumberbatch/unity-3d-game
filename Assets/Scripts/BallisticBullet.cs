688ede351b24f570b7c186b3bd68ac54517851a2
cucumberbatch <ultraresonance@gmail.com>
2021-05-04 04:10:35 +0600

    Пуля стала баллистическим объектом, необходим дебаг расчета отражений

---
+++ Assets/Scripts/BallisticBullet.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallisticBullet : MonoBehaviour
+{
+	public float     speed;
+	public float     mass;
+	public float     gravityFactor;
+	public Vector3   normalMovementDirection;
+	public Transform originTransform;
+	public float 	 rayDuration;
+
+	private Vector3 _previousPosition;
+	
+
+	Vector3 GenerateRandomBrightColorVector() 
+	{
+		return new Vector3(
+			Random.Range(0.0f, 1.0f), 
+			Random.Range(0.0f, 1.0f), 
+			Random.Range(0.0f, 1.0f)
+		   ).normalized;
+	}
+
+	void Start()
+	{
+		_previousPosition = originTransform.position;
+	}
+
+	void Update()
+	{
+		// Set-up for drawing a colored path of bullet movement
+		Vector3 colorVector 	= GenerateRandomBrightColorVector();
+		Color 	generatedColor	= new Color(
+				colorVector.x, 
+				colorVector.y, 
+				colorVector.z
+		);
+		
+		Vector3 incidentBeamDirection	= transform.position - _previousPosition;
+		normalMovementDirection 	= incidentBeamDirection.normalized;
+
+		Debug.DrawRay(_previousPosition, incidentBeamDirection, generatedColor, rayDuration);
+
+		
+		// Raycasting for ricochete path calculations
+		RaycastHit hit;
+	
+		if (Physics.Raycast(_previousPosition, incidentBeamDirection, out hit, incidentBeamDirection.magnitude))
+		{
+			incidentBeamDirection	= hit.point - _previousPosition;
+			Vector3 reflectedBeamDirection	= Vector3.Reflect(incidentBeamDirection, hit.normal);
+
+			// Debug.DrawRay(_previousPosition, incidentBeamDirection, generatedColor, rayDuration);
+
+			// transform.position	= Vector3.Reflect(transform.position - hit.point, hit.normal);
+			incidentBeamDirection	= reflectedBeamDirection;
+			_previousPosition	= hit.point;
+		}
+		else
+		{
+			_previousPosition   = transform.position;
+		}
+		
+		// Rewrite position values of a bullet
+		transform.position += incidentBeamDirection.normalized * speed * Time.deltaTime + (Vector3.down * mass * gravityFactor * Time.deltaTime * Time.deltaTime);
+	}
+}
--- Assets/Scripts/CharacterInventory.cs
+++ Assets/Scripts/CharacterInventory.cs
@@ -15,22 +15,23 @@ public class CharacterInventory : MonoBehaviour
 
     void Update()
     {
-		Color 	redColor 	   		   = new Color(1, 0, 0);
-		Vector3 originPosition		   = transform.position;
-		Vector3 incidentBeamDirection  = transform.rotation * new Vector3(0, 0, 1);
-		Vector3 reflectedBeamDirection = Vector3.zero;
+	/*
+		Color 	redColor		= new Color(1, 0, 0);
+		Vector3 originPosition		= transform.position;
+		Vector3 incidentBeamDirection	= transform.rotation * new Vector3(0, 0, 1);
+		Vector3 reflectedBeamDirection	= Vector3.zero;
 
 		RaycastHit hit;
 
 		while (Physics.Raycast(originPosition, incidentBeamDirection, out hit))
 		{
-			incidentBeamDirection  = hit.point - originPosition;
-			reflectedBeamDirection = Vector3.Reflect(incidentBeamDirection, hit.normal);
+			incidentBeamDirection	= hit.point - originPosition;
+			reflectedBeamDirection	= Vector3.Reflect(incidentBeamDirection, hit.normal);
 
 			Debug.DrawRay(originPosition, incidentBeamDirection, redColor);
 
-			incidentBeamDirection = reflectedBeamDirection;
-			originPosition 		  = hit.point;
+			incidentBeamDirection	= reflectedBeamDirection;
+			originPosition		= hit.point;
 		}
 
 		if (reflectedBeamDirection != Vector3.zero)
@@ -38,12 +39,13 @@ public class CharacterInventory : MonoBehaviour
 			Debug.DrawRay(originPosition, reflectedBeamDirection, redColor);
 		}
 
+	*/
 
-	    if (Input.GetKeyDown(KeyCode.T)) {
-        	isTwoHanded = !isTwoHanded;
-			secondGun.SetActive(isTwoHanded);
-			firstGun.SetActive(!isTwoHanded);
+ 	if (Input.GetKeyDown(KeyCode.T)) {
+		isTwoHanded = !isTwoHanded;
+		secondGun.SetActive(isTwoHanded);
+		firstGun.SetActive(!isTwoHanded);
         }
-
+	
     }
 }
