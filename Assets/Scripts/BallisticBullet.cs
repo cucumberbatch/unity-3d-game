7be09bdc5d8695a327312ce4c69ba64680c06f56
cucumberbatch <ultraresonance@gmail.com>
2021-05-04 21:41:22 +0600

    Снаряды вылетают в направлении взгляда за счет BallisticGun, вынесен префаб пули

--- Assets/Scripts/BallisticBullet.cs
+++ Assets/Scripts/BallisticBullet.cs
@@ -4,56 +4,51 @@ using UnityEngine;
 
 public class BallisticBullet : MonoBehaviour
 {
-	public Transform originTransform;
+	public BallisticGun	gun;
+	public Transform 	originTransform;
 	
 	public float     speed;
 	public float     mass;
 	public float     gravityFactor;
 	public float 	 rayDuration;
 
-	private Vector3 _previousPosition;
-	
+	private Vector3 	_previousPosition;
+	private RaycastHit 	_hit;
+	private Vector3		_incidentBeamDirection;
 
-	Vector3 GenerateRandomBrightColorVector() 
+
+	public void SetGunReference(BallisticGun gun)
 	{
-		return new Vector3(
-			Random.Range(0.0f, 1.0f), 
-			Random.Range(0.0f, 1.0f), 
-			Random.Range(0.0f, 1.0f)
-		   ).normalized;
+		this.gun = gun;
+	}
+
+	Color GenerateRandomBrightColorVector() 
+	{
+		Vector3 colorVector = new Vector3(
+				Random.Range(0.0f, 1.0f), 
+				Random.Range(0.0f, 1.0f), 
+				Random.Range(0.0f, 1.0f)
+			).normalized;
+		
+		return new Color(colorVector.x, colorVector.y, colorVector.z);
 	}
 
 	void Start()
 	{
-		_previousPosition = originTransform.position;
+		_previousPosition = gun.bulletEmitterTransform.position;
 	}
 
 	void Update()
 	{
-		// Set-up for drawing a colored path of bullet movement
-		Vector3 colorVector 	= GenerateRandomBrightColorVector();
-		Color 	generatedColor	= new Color(
-				colorVector.x, 
-				colorVector.y, 
-				colorVector.z
-		);
-		
-		Vector3 incidentBeamDirection	= transform.position - _previousPosition;
-		Vector3 reflectedBeamDirection;	 	
-
-		Debug.DrawRay(_previousPosition, incidentBeamDirection, generatedColor, rayDuration);
-
+		// Find out the movement direction
+		_incidentBeamDirection	= transform.position - _previousPosition;
 		
 		// Raycasting for ricochete path calculations
-		RaycastHit hit;
-
-		if (Physics.Raycast(_previousPosition, incidentBeamDirection, out hit, incidentBeamDirection.magnitude))
+		if (Physics.Raycast(_previousPosition, _incidentBeamDirection, out _hit, _incidentBeamDirection.magnitude))
 		{
-			incidentBeamDirection	= hit.point - _previousPosition;
-			reflectedBeamDirection	= Vector3.Reflect(incidentBeamDirection, hit.normal);
-			incidentBeamDirection	= reflectedBeamDirection;
-			transform.position	= hit.point + incidentBeamDirection;
-			_previousPosition	= hit.point;
+			_incidentBeamDirection	= Vector3.Reflect(_hit.point - _previousPosition, _hit.normal);
+			_previousPosition	= _hit.point;
+			transform.position	= _previousPosition + _incidentBeamDirection;
 		}
 		else
 		{
@@ -61,6 +56,13 @@ public class BallisticBullet : MonoBehaviour
 		}
 		
 		// Rewrite position values of a bullet
-		transform.position += incidentBeamDirection.normalized * speed * Time.deltaTime + (Vector3.down * mass * gravityFactor * Time.deltaTime * Time.deltaTime);
+		transform.position += _incidentBeamDirection.normalized * speed * Time.deltaTime + (Vector3.down * mass * gravityFactor * Time.deltaTime * Time.deltaTime);
+
+		// Set-up for drawing a colored path of bullet movement
+		Color 	generatedColor	= GenerateRandomBrightColorVector();
+
+		// Draw bullet trace (uncomment for debug)
+		// Debug.DrawRay(_previousPosition, _incidentBeamDirection, generatedColor, rayDuration);
+
 	}
 }
---
+++ Assets/Scripts/BallisticGun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallisticGun : MonoBehaviour
+{
+	public Camera 		playerCamera;
+	public GameObject 	bullet;
+	public Transform 	bulletEmitterTransform;
+
+	private RaycastHit 	_hit;
+	private Ray		_ray;
+
+	void Update()
+	{
+        	_ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+
+		// Instantly emmit bullets on left mouse click	
+		if (Input.GetMouseButton(0))
+		{
+			bullet 	= Instantiate(bullet, _ray.origin + _ray.direction * 2, Quaternion.FromToRotation(-Vector3.forward, _hit.normal));
+
+			bullet.GetComponent<BallisticBullet>().SetGunReference(this);
+		}
+		
+		// Draw ray in look direction (uncomment for debug)
+		// Debug.DrawRay(_ray.origin, _ray.direction * 5, Color.red);
+	}
+}
